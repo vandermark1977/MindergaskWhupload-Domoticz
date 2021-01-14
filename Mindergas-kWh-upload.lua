afbcf89a629d9f862df48cdb4109e8d35a72cdf5
vandermark1977 <49677940+vandermark1977@users.noreply.github.com>
2021-01-14 13:50:36 +0100

    Update Mindergas-kWh-upload.lua

--- Mindergas-kWh-upload.lua
+++ Mindergas-kWh-upload.lua
@@ -14,9 +14,8 @@ return {
 	on = {
 		timer 		= {'at 0:12'},       --Time for daily upload kWh counter value to mindergas.nl
 		httpResponses 	= {'UploadToMindergas'}},
-	 logging =    
-    {   
-        level = domoticz.LOG_ERROR, -- change to LOG_ERROR when OK - was LOG_DEBUG
+	 logging =    {
+		level = domoticz.LOG_ERROR, -- change to LOG_ERROR when OK - was LOG_DEBUG
         marker = scriptVar,
     },
 	execute = function(domoticz, item)
